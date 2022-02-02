cb5edb64d1ae2ccf8fe35348b2edc7c1310c784e
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-02-02 20:03:01 +0100

    1.5.2 move Node to IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,3 +38,40 @@ public class IntLinkedList {
 	}
 
 }
+
+
+//kombinacja klawiszy alt + strzalka lewo/prawo na Windowsie
+//pozwala przeskakiwac pomiedzy otwartymi edytorami w IDE wg kolejnosci ich odwiedzenia
+//na macOS ekwiwalent to option + command + strzalka lewo/prawo
+
+class Node {
+
+	private int value;
+	private Node prev;
+	private Node next;
+
+	public Node(int i) {
+		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+}
